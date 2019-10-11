bc2926dd0d130a3edbccfea8e443042ee3147312
Jiří Šmolík <jiri.smolik@ackee.cz>
2019-10-11 16:11:49 +0200

    Setup configuration loader
     #38752

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,25 @@
 import { createLoader, safeValues, values } from 'configuru';
+import { SUPPORTED_REGIONS } from 'firebase-functions';
+import * as fs from 'fs';
+import * as path from 'path';
 import { Level } from 'pino';
 
-const loader = createLoader();
+const PROJECT_ROOT = path.join(__dirname, '..', '..');
+const loaderOptions = {
+    // All options must be specified, including defaults
+    // See https://github.com/AckeeCZ/configuru/issues/10
+    defaultConfigPath: path.join(PROJECT_ROOT, '.env.json'),
+    userConfigPath: fs.existsSync(path.join(PROJECT_ROOT, 'credentials.json'))
+        ? path.join(PROJECT_ROOT, 'credentials.json')
+        : process.env.CFG_JSON_PATH || path.join(PROJECT_ROOT, '.env.json'),
+    envMode: 'all' as const,
+};
+
+const loader = createLoader(loaderOptions);
+
+export type CfRegion = typeof SUPPORTED_REGIONS[number];
+
+const cfRegionLoader = loader.custom((x: CfRegion) => x);
 
 const configSchema = {
     logger: {
@@ -12,8 +30,11 @@ const configSchema = {
     node: {
         env: loader.string('NODE_ENV'),
     },
+    functions: {
+        region: cfRegionLoader('CF_REGION'),
+    },
     server: {
-        port: loader.number('SERVER_PORT'),
+        port: loader.number.nullable('SERVER_PORT'),
     },
 };
 
