59dfd9ea688f486f8f08898684ca4bb5fe59f279
Jiří Šmolík <jiri.smolik@ackee.cz>
2019-10-11 16:23:32 +0200

    Remove unnecessary files
     #38752

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import logger from 'app/logger';
-import { createServer } from 'unicore';
-import bindRoutes from './config/routes';
-
-const server = createServer();
-server.use(logger.express);
-bindRoutes(server);
-
-export default server;
