5117985c27b0e02850cb1d5dbd247d3304ef80ee
Jaroslav Šmolík <grissius@gmail.com>
2020-06-01 09:22:24 +0200

    ✨ Add dangerfile

---
+++ dangerfile.ts
@@ -0,0 +1,16 @@
+import {
+  danger,
+  warn,
+  message,
+  fail,
+  markdown,
+  schedule,
+  peril,
+  results,
+} from 'danger'
+import { runDangerRules } from '@ackee/styleguide-backend-config/danger'
+
+runDangerRules(
+  { danger, warn, message, fail, markdown, schedule, peril, results },
+  { validSpellingWords: [] }
+)
